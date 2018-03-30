c160b2858d3cec3fa87bfe0b923ba0a7c28b98f7
James Russo <1jlrusso@gmail.com>
2018-03-30 16:08:22 -0700

    replaced querySelectorAll - input[type='image'] with img

--- displacement.js
+++ displacement.js
@@ -126,7 +126,7 @@
 
 		/*--- Toggle Img Caption Show/Hide ---*/
 		var imgCaptions = document.getElementsByClassName("img-caption");
-		var thirdPageImgs = document.querySelectorAll(".third-page-pics > input[type='image']");
+		var thirdPageImgs = document.querySelectorAll(".third-page-pics > img");
 		thirdPageImgs.forEach(function(image){
 		  image.addEventListener("click", function(e){
 		    var imgCaption = this.nextElementSibling;
